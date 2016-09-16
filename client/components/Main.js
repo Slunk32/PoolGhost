fdeca6a99a42bed0f069cbe0ed83191c3d8c4b59
Russell Salerno <russell.j.salerno@gmail.com>
2016-09-16 10:01:33 -0700

    added gitignore for node_modules, and set up a nav bar component

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import NavbarInstance from './Nav';
 
 const Main = React.createClass({
   render() {
     return (
       <div>
+      <NavbarInstance />
         <h1>
           <Link to="/">Pool Ghost</Link>
         </h1>
