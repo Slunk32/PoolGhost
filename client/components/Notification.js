84c8c1907aa63090331a4c292bb3e03312375d7a
Russell Salerno <russell.j.salerno@gmail.com>
2016-09-27 15:35:13 -0700

    banner styling

---
+++ client/components/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Notification = React.createClass({
+  render() {
+    return (
+      <div className="notification-box">
+       <span className="banner"> blah blasdsadsadkadasd </span>
+      </div>
+    )
+  }
+})
+
+
+
+export default Notification;
